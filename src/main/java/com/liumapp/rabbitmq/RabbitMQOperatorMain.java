7d1b78a2fc3c8e693ea46aabe18627dc71b279b3
liumapp <liumapp.com@gmail.com>
2018-08-17 10:17:38 +0800

    add necessary requires

---
+++ src/main/java/com/liumapp/rabbitmq/RabbitMQOperatorMain.java
@@ -0,0 +1,14 @@
+package com.liumapp.rabbitmq;
+
+/**
+ * @author liumapp
+ * @file RabbitMQOperatorMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/17
+ */
+public class RabbitMQOperatorMain {
+
+
+
+}
