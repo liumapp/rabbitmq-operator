d35f915d13470e6aad639d9135f424d2322c2727
liumapp <liumapp.com@gmail.com>
2018-09-30 23:31:56 +0800

    ready to run demo1

--- demo/src/main/java/com/liumapp/demo/rabbitmq/operator/consumer/Demo1Consumer.java
+++ demo/src/main/java/com/liumapp/demo/rabbitmq/operator/consumer/Demo1Consumer.java
@@ -1,5 +1,10 @@
 package com.liumapp.demo.rabbitmq.operator.consumer;
 
+import com.liumapp.demo.rabbitmq.operator.pattern.Demo1Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 /**
  * file Demo1Consumer.java
  * author liumapp
@@ -8,8 +13,13 @@ package com.liumapp.demo.rabbitmq.operator.consumer;
  * homepage http://www.liumapp.com
  * date 2018/9/30
  */
+@Service
 public class Demo1Consumer {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    public void handle (Demo1Pattern demo1Pattern) {
+        logger.info("demo1 consumer service get pattern : " + demo1Pattern.toString());
+    }
 
 }
--- demo/src/main/java/com/liumapp/demo/rabbitmq/operator/publisher/Demo1Publisher.java
+++ demo/src/main/java/com/liumapp/demo/rabbitmq/operator/publisher/Demo1Publisher.java
@@ -1,9 +1,11 @@
 package com.liumapp.demo.rabbitmq.operator.publisher;
 
+import com.liumapp.demo.rabbitmq.operator.pattern.Demo1Pattern;
 import com.liumapp.rabbitmq.publisher.BasicPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.support.CorrelationData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,17 +21,29 @@ public class Demo1Publisher extends BasicPublisher {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public void send () {
+    @Autowired
+    private Demo1Pattern demo1Pattern;
 
+    @Override
+    public void send () {
+        demo1Pattern.setMsg("this is demo1 publisher");
+        logger.info("demo1 publisher send msg : " + demo1Pattern.getMsg());
+        this.sendMessage("demo1Consumer", "handle", Integer.toString(10001), demo1Pattern);
     }
 
     @Override
     public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-
+        logger.info("demo1 publisher get confirmed info : " + correlationData);
+        if (ack) {
+            logger.info("send msg success");
+        } else {
+            logger.error("send msg failed and the reason is : " + cause);
+        }
     }
 
     @Override
     public void setCallBack() {
-
+        rabbitTemplate.setReturnCallback(this);
+        rabbitTemplate.setConfirmCallback(this);
     }
 }
